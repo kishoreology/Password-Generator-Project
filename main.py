93c0c1bc577610e6deda3e083a1cab395cff234f
kishore <106666554+kishoreology@users.noreply.github.com>
2024-04-23 20:39:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ for char in range(1, nr_symbols + 1):
   password_list.append(random.choice(symbols))
 for char in range(1, nr_numbers + 1):
   password_list.append(random.choice(numbers))
+random.shuffle(password_list)
 password = ""
 for char in password_list:
   password += char
